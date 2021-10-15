760e036041dfa2f29bf036cfd11b3ac25d005cbc
pratheepr <pratheepr@gmail.com>
2021-10-15 21:38:20 +0200

    Initial commit

---
+++ LucaDB/DBAccess.py
@@ -0,0 +1,259 @@
+import sqlite3
+from datetime import datetime, date
+
+
+def CrtConnObject(DB_Location):
+    try:
+        sqliteConnection = sqlite3.connect(DB_Location, detect_types=sqlite3.PARSE_DECLTYPES)
+        cursor = sqliteConnection.cursor()
+        return sqliteConnection
+    except sqlite3.Error as ERROR:
+        print("Error while working with SQLite", ERROR)
+
+
+# function  to insert data into alerting rules
+def AlertingRules_Insert(ConnObj, Buss_area, Alarm_name, Alarm_desc, Tag_name, Tag_condition, Threshold_value,
+                         Pcntg_Above_Threshold, Check_duration_secs, Multi_cond, Logic_flow_order,
+                         Logical_operator, Alert_active, Suppress_after_alert_secs, Alert_recepients):
+    db_msg = ''
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_alerting_rules_insert_param = "INSERT INTO ALERTING_RULES (BUSS_AREA, ALARM_DESC, ALARM_NAME, TAG_NAME, TAG_CONDITION, THRESHOLD_VALUE, PCNTG_ABOVE_THRESHOLD, CHECK_DURATION_IN_SECS," \
+                                             "MULTI_COND, LOGIC_FLOW_ORDER, LOGICAL_OPERATOR, ALERT_ACTIVE, SUPPRESS_AFTR_ALERT_IN_SECS, ALERT_RECEPIENTS) " \
+                                             "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "
+        sqlite_alerting_rules_data_tuple = (
+            Buss_area, Alarm_name, Alarm_desc, Tag_name, Tag_condition, Threshold_value, Pcntg_Above_Threshold,
+            Check_duration_secs, Multi_cond,
+            Logic_flow_order, Logical_operator, Alert_active, Suppress_after_alert_secs, Alert_recepients)
+
+        CursorObj.execute(sqlite_alerting_rules_insert_param, sqlite_alerting_rules_data_tuple)
+        ConnObj.commit()
+
+        CursorObj.execute("select * from ALERTING_RULES")
+        for row in CursorObj:
+            print(row)
+
+        # CursorObj.close()
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite at alerting rules table ", error)
+        db_msg = 'failure'
+
+    return db_msg
+
+
+def AlertingRules_Select(ConnObj):
+    db_msg = ''
+    records = []
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_alertingrules_select_qry = """SELECT SID, BUSS_AREA, ALARM_NAME, ALARM_DESC, TAG_NAME, TAG_CONDITION, THRESHOLD_VALUE, PCNTG_ABOVE_THRESHOLD,
+                                            CHECK_DURATION_IN_SECS, MULTI_COND, LOGIC_FLOW_ORDER, LOGICAL_OPERATOR, ALERT_ACTIVE,
+                                            SUPPRESS_AFTR_ALERT_IN_SECS, ALERT_RECEPIENTS FROM ALERTING_RULES WHERE ALERT_ACTIVE='Y'"""
+
+        print(sqlite_alertingrules_select_qry)
+
+        ret = CursorObj.execute(sqlite_alertingrules_select_qry)
+        records = CursorObj.fetchall()
+
+        CursorObj.close()
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite at Opc trans table", error)
+        # db_msg = 'Failure: ' + error
+
+    return records
+
+
+# function tto insert data into alerts table
+def Alerts_Insert(ConnObj, Alerting_Rules_sid, Alert_Conditiion, Actual_Value, Alert_Mail_Sent, Alert_Comments):
+    db_msg = ''
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_alerts_insert_param = "INSERT INTO ALERTS(ALERTING_RULES_SID, ALERT_CONDITION, ACTUAL_VALUE, LOAD_DATETIME, ALERT_MAIL_SENT, ALERT_COMMENTS ) " \
+                                     "VALUES(?, ?, ?, ?, ?, ?) "
+        sqlite_alerts_data_tuple = (
+            Alerting_Rules_sid, Alert_Conditiion, Actual_Value, datetime.now(), Alert_Mail_Sent, Alert_Comments)
+
+        CursorObj.execute(sqlite_alerts_insert_param, sqlite_alerts_data_tuple)
+        ConnObj.commit()
+
+        CursorObj.close()
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite at alerts table", error)
+        db_msg = 'failure'
+
+    return db_msg
+
+
+# function to delete records less than the given input date
+def Alerts_Purge(ConnObj, input_date):
+    db_msg = ''
+    format = '%Y-%m-%d'  # suitable  format
+    input_date = datetime.strptime(input_date, format)
+    Delete_Sql = "DELETE FROM ALERTS WHERE DATE(LOAD_DATETIME) <= ? "
+    param = [input_date]
+
+    try:
+        CursorObj = ConnObj.cursor()
+        CursorObj.execute(Delete_Sql, param)
+        ConnObj.commit()
+        CursorObj.close()
+        db_msg = 'success'
+    except sqlite3.Error as PurgeErr:
+        print("Error in purging data", PurgeErr)
+        db_msg = 'failure'
+
+    return db_msg
+
+
+# function to insert data into opc_trans_log table
+
+def OpcTransLog_Insert(ConnObj, Opc_tag, Tag_value, Tag_status, Readreq_Timestamp):
+    db_msg = ''
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_opc_log_insert_param = "INSERT INTO OPC_TRANS_LOG(OPC_TAG, TAG_VALUE, TAG_STATUS, OPC_TIMESTAMP, READREQ_TIMESTAMP) " \
+                                      "VALUES(?, ?, ?, ?, ?) "
+        sqlite_opc_log_data_tuple = (Opc_tag, Tag_value, Tag_status, datetime.now(), Readreq_Timestamp)
+
+        CursorObj.execute(sqlite_opc_log_insert_param, sqlite_opc_log_data_tuple)
+        ConnObj.commit()
+
+        CursorObj.close()
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite at Opc trans table", error)
+        db_msg = 'Failure: ' + error
+
+    return db_msg
+
+
+def OpcTransLog_MassInsert(ConnObj, DF_writeToDB):
+    db_msg = ''
+    try:
+        DF_writeToDB.to_sql('OPC_TRANS_LOG', con=ConnObj, if_exists='append', index=False)
+        ConnObj.commit()
+
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while performing mass insert into OPC_TRANS_LOG", error)
+        db_msg = 'Failure: ' + error
+
+    return db_msg
+
+
+def OpcTransLog_Select(ConnObj, No_Of_Days):
+    db_msg = ''
+    records = []
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_opctranslog_select_qry = """SELECT SID, OPC_TAG, TAG_VALUE, TAG_STATUS, LOAD_TIMESTAMP FROM OPC_TRANS_LOG """ \
+                                        "WHERE (LOAD_TIMESTAMP) > DATETIME('now','-" + str(No_Of_Days) + " day')"""
+
+        print(sqlite_opctranslog_select_qry)
+
+        ret = CursorObj.execute(sqlite_opctranslog_select_qry)
+        records = CursorObj.fetchall()
+
+        CursorObj.close()
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite at Opc trans table", error)
+        # db_msg = 'Failure: ' + error
+
+    return records
+
+
+# function to purge data from opc_trans_table
+def OpcTransLog_Purge(ConnObj, input_date):
+    db_msg = ''
+    format = '%Y-%m-%d'  # suitable  format
+    input_date = datetime.strptime(input_date, format)
+    Delete_Sql = "DELETE FROM OPC_TRANS_LOG WHERE DATE(LOAD_DATETIME) <= ? "
+    param = [input_date]
+
+    try:
+        CursorObj = ConnObj.cursor()
+        CursorObj.execute(Delete_Sql, param)
+        ConnObj.commit()
+        CursorObj.close()
+        db_msg = 'success'
+    except sqlite3.Error as PurgeErr:
+        print("Error in purging data", PurgeErr)
+        db_msg = 'failure'
+
+    return db_msg
+
+
+def OpcTagMaster_Select(ConnObj):
+    db_msg = ''
+    records = []
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_opctagsmaster_select_qry = """SELECT SID, OPC_TAGNAME, OPC_TAG_DESC, INSERT_USER, INSERT_DATE, TAG_ACTIVE FROM OPC_TAGS_MASTER """
+
+        print(sqlite_opctagsmaster_select_qry)
+
+        ret = CursorObj.execute(sqlite_opctagsmaster_select_qry)
+        records = CursorObj.fetchall()
+
+        CursorObj.close()
+
+        db_msg = 'success'
+    except sqlite3.Error as Err:
+        print("Error in selecting OpcTagMaster data", Err)
+        db_msg = 'failure' + Err
+
+    finally:
+        if db_msg == 'success':
+            return records
+        else:
+            return []
+
+def OpcTagMaster_Insert(ConnObj, Opc_TagName, Opc_Tag_Desc, Insert_User, Tag_Active):
+    db_msg = ''
+    try:
+        CursorObj = ConnObj.cursor()
+
+        sqlite_opc_Tag_Mstr_insert_param = "INSERT INTO OPC_TRANS_LOG(OPC_TAGNAME, OPC_TAG_DESC, INSERT_USER, " \
+                                           "INSERT_DATE, TAG_ACTIVE) " \
+                                           "VALUES(?, ?, ?, ?) "
+        sqlite_opc_Tag_Mstr_data_tuple = (Opc_TagName, Opc_Tag_Desc, Insert_User, datetime.now(), Tag_Active)
+
+        CursorObj.execute(sqlite_opc_Tag_Mstr_insert_param, sqlite_opc_Tag_Mstr_data_tuple)
+        ConnObj.commit()
+
+        CursorObj.close()
+        db_msg = 'success'
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite at Opc Tag Master", error)
+        db_msg = 'Failure: ' + error
+
+    return db_msg
+
+
+if __name__ == "__main__":
+    try:
+        InsertConn = CrtConnObject()
+
+    except sqlite3.Error as error:
+        print("Error while working with SQLite", error)
+    finally:
+        if InsertConn:
+            InsertConn.close()
+            print("SQLite is closed")
---
+++ OpcRead.py
@@ -0,0 +1,177 @@
+import pywintypes
+import OpenOPC
+import sys
+import time as tm
+import pandas as pd
+import math
+import numpy as np
+from configparser import ConfigParser
+from datetime import datetime
+from os import path
+from LucaDB import DBAccess as db
+
+
+def tags_listing(DBConnObj, nof_tags_per_line, err_tag_list):
+    # my_file = open(tag_file, 'r')
+    # my_file_list = my_file.read().splitlines()
+
+    OPC_Tags_Read = db.OpcTagMaster_Select(ConnObj=DBConnObj)
+
+    Proc_OPC_Tags = list(filter(lambda OPC_Tags_Read: OPC_Tags_Read[5] == 'Y', OPC_Tags_Read))
+
+    nof_rows = math.ceil(len(Proc_OPC_Tags) / nof_tags_per_line)
+    db_tag_list = [itm[1] for itm in Proc_OPC_Tags if itm[1] not in err_tag_list]
+
+    ret_tag_list = []
+    ctr = 0
+
+    for x in range(nof_rows):
+        col = []
+        for y in range(nof_tags_per_line):
+            if ctr >= len(db_tag_list):
+                col.append('')
+            else:
+                col.append(db_tag_list[ctr])
+            ctr = ctr + 1
+
+        ret_tag_list.append(col)
+
+    return ret_tag_list
+
+
+def ConnectOPC(OPC_Server_Name):
+    try:
+        OPC_Client = OpenOPC.client()
+        ret_value = 0
+        print(OPC_Client.servers())
+    except Exception as e:
+        print('Exception in OPC Connect' + str(e))
+        ret_value: Exception = e
+    finally:
+        return OPC_Client, ret_value
+
+
+def Cleanup_Metrics_File(opc_read_output_filename, history_duration_in_hours):
+    print('Cleanup Metrics function')
+    df_metrics_file = pd.read_csv(opc_read_output_filename, delimiter=',', header=0, parse_dates=['datetime'],
+                                  infer_datetime_format=True)
+    print(df_metrics_file)
+    # df['new_datetime'] = pd.to_datetime(df['datetime'], infer_datetime_format=True)
+    print(df_metrics_file.info())
+
+    df_fil = df_metrics_file[df_metrics_file['datetime'] >= datetime.now() - pd.Timedelta(history_duration_in_hours)]
+    df_fil.to_csv(opc_read_output_filename, index=False, header=True, mode='w')
+
+
+def Write_To_Files(ctr, df_write, opc_read_output_filename):
+    print(df_write)
+    df_pivot = df_write.pivot_table(index=['datetime'], columns=['opc_tag'], values='value', aggfunc=sum)
+
+    if ctr == 0:
+        df_write.to_csv(opc_read_output_filename, index=False, header=True, mode='w')
+        df_pivot.to_csv(opc_read_pivot_output_filename, index=True, header=True, mode='w')
+    else:
+        df_write.to_csv(opc_read_output_filename, index=False, header=False, mode='a')
+        df_pivot.to_csv(opc_read_pivot_output_filename, index=True, header=False, mode='a')
+
+    return
+
+
+if __name__ == "__main__":
+    # arguments = len(sys.argv) - 1
+
+    ConfigFile = 'opcread_config.ini'
+    config = ConfigParser()
+    config.read(ConfigFile)
+
+    opc_read_tags_filename = config['opc_read']['opc_read_tags_filename']
+    history_duration_in_hours = config['opc_read']['history_duration_in_hours']
+    opc_read_output_filename = config['opc_read']['opc_read_output_filename']
+    tag_listing_per_call = int(config['opc_read']['tag_listing_per_call'])
+    OPC_Server_Name = config['opc_read']['OPC_Server_Name']
+    sleep_duration_in_secs = int(config['opc_read']['sleep_duration_in_secs'])
+    opc_read_pivot_output_filename = config['opc_read']['opc_read_pivot_output_filename']
+    alert_db_location = config['opc_read']['DB_Location']
+    opc_error_retry = int(config['opc_read']['opc_error_retry'])
+
+    OPC_Conn_Obj, ret = ConnectOPC(OPC_Server_Name)
+    OPC_Conn_Obj.connect(OPC_Server_Name)
+    #    print("The script is called with arguments" + str(arguments))
+
+    print('Connect established')
+    print(ret)
+    print(OPC_Conn_Obj)
+    pywintypes.datetime = pywintypes.TimeType
+
+    DbConnObj = db.CrtConnObject(alert_db_location)
+
+    print('tag function')
+    # tag_list = tags_listing(opc_read_tags_filename, tag_listing_per_call)
+    tag_list = tags_listing(DBConnObj=DbConnObj, nof_tags_per_line=tag_listing_per_call, err_tag_list=[])
+    print(f'********************Tags Selected are: {tag_list}')
+    df_error_tags = pd.DataFrame(columns=['OPC_TAG', 'READREQ_TIMESTAMP'])
+
+    ctr = 0
+    while 1:
+        print('CTR is : ' + str(ctr))
+        print('\n ******Read INDIVIDUAL Tags')
+        OPC_Conn_Obj, ret = ConnectOPC(OPC_Server_Name)
+        OPC_Conn_Obj.connect(OPC_Server_Name)
+        Error_Tag_Found = 0
+        for OPC_Tags in tag_list:
+            try:
+                Readreq_Timestamp = datetime.now()
+                read_ret = OPC_Conn_Obj.read(OPC_Tags)
+                df = pd.DataFrame(read_ret, columns=['opc_tag', 'value', 'quality', 'old_datetime'])
+                df['old_datetime'] = df['old_datetime'].str[:19]
+                df = df.loc[df['opc_tag'] != '']
+                df['datetime'] = pd.to_datetime(df['old_datetime'], infer_datetime_format=True)
+                df['value'].replace({'True': 1, 'False': 0})
+                df = df.drop(columns=['old_datetime'])
+                df['Readreq_Timestamp'] = Readreq_Timestamp
+
+                # print(df.info())
+                df_tmp = [df['opc_tag'], df['value'], df['quality'], df['datetime'], df['Readreq_Timestamp']]
+                headers = ['OPC_TAG', 'TAG_VALUE', 'TAG_STATUS', 'OPC_TIMESTAMP', 'READREQ_TIMESTAMP']
+
+                df_cp_todb = pd.concat(df_tmp, axis=1, keys=headers)
+
+                try:
+                    ret = db.OpcTransLog_MassInsert(ConnObj=DbConnObj, DF_writeToDB=df_cp_todb)
+                except Exception as e:
+                    print(e)
+
+                df_tmp_err_tags = df_cp_todb.loc[df_cp_todb['TAG_STATUS'] == 'Error']
+                if df_tmp_err_tags.shape[0] > 0:
+                    df_t = pd.DataFrame([df_tmp_err_tags['OPC_TAG'], df_tmp_err_tags['READREQ_TIMESTAMP']])
+                    df_error_tags = df_error_tags.append(df_tmp_err_tags)
+                    Error_Tag_Found = 1
+
+                if df_error_tags.shape[0] > 0:
+                    df_error_tags = df_error_tags.loc[
+                        df_error_tags['READREQ_TIMESTAMP'] > datetime.now() - pd.Timedelta(seconds=opc_error_retry)]
+
+                    print(' Error Data Frame : *******************')
+                    print(df_error_tags)
+                    lst_err_tags = list(df_error_tags['OPC_TAG'].unique())
+                    print(f'Distinct error tags are : {lst_err_tags}  ')
+                    print(' ************************************** :')
+
+
+                # Write_To_Files(ctr=ctr, df_write=df, opc_read_output_filename=opc_read_output_filename)
+
+            except Exception as e:
+                print('Error in OPC_Conn_Obj.read(OPC_Tags)')
+                print(e)
+
+        ctr = ctr + 1
+        tm.sleep(sleep_duration_in_secs)
+
+        if ctr % 5 == 0 or Error_Tag_Found == 1:
+            tag_list = tags_listing(DBConnObj=DbConnObj, nof_tags_per_line=tag_listing_per_call,
+                                    err_tag_list=lst_err_tags)
+            print(f'********************Tags Selected are: {tag_list}')
+
+        # if ctr % 10 == 0:
+        #    Cleanup_Metrics_File(opc_read_output_filename, history_duration_in_hours)
+        # Cleanup_Metrics_File(opc_read_pivot_output_filename,history_duration_in_hours)
